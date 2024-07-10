ce1db5ac5f674494d295c250c7b479c92e282a74
Hashim Aziz Muhammad <hashimazizm@gmail.com>
2024-07-10 16:18:39 +0200

    commit bank_account.py

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current balance: ${self.account_balance:.2f}")
